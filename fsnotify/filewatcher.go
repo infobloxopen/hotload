a9750df16b61a443eb9501a4e65bbe5a7783c646
Daniel García <dgarcia@infoblox.com>
2024-02-28 00:50:26 -0600

    add filename to error returned on config read (#27)
    * add filename to error returned on config read
    
    * Update fsnotify/filewatcher.go
    
    Co-authored-by: Yugandhar Vardhineni <yvardhineni@infoblox.com>
    
    * remove unnecessary import after commiting feedback
    
    ---------
    
    Co-authored-by: Yugandhar Vardhineni <yvardhineni@infoblox.com>

--- fsnotify/filewatcher.go
+++ fsnotify/filewatcher.go
@@ -11,6 +11,7 @@ import (
 
 	rfsnotify "github.com/fsnotify/fsnotify"
 	"github.com/infobloxopen/hotload"
+	"github.com/pkg/errors"
 )
 
 func init() {
@@ -44,7 +45,7 @@ type pathWatch struct {
 func readConfigFile(path string) (v []byte, err error) {
 	v, err = ioutil.ReadFile(path)
 	if err != nil {
-		return
+		return nil, errors.Wrapf(err, "could not read %v", path)
 	}
 	v = []byte(strings.TrimSpace(string(v)))
 	return
