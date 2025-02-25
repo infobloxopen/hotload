e3e7a6132b06663398147a42c014e4bdd6abcc95
rchowinfoblox <rchow@infoblox.com>
2025-02-25 14:41:12 -0800

    PTOCP-2229: Add new ModTimeMonitor pkg/api to monitor modtime of paths

---
+++ fsnotify/modtime/modtime_monitor.go
@@ -0,0 +1,172 @@
+package modtime
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"path"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/infobloxopen/hotload/logger"
+)
+
+var (
+	ErrDuplicatePath = errors.New("duplicate path")
+	ErrPathNotFound  = errors.New("path not found")
+)
+
+// ModTimeMonitor monitors the modtimes for a set of paths in a filesystem
+type ModTimeMonitor struct {
+	sync.RWMutex // used to synchronize changes to the set of paths being monitored
+	log          logger.Logger
+	statFS       fs.StatFS // use io/fs.FS so we can mock FileSystem for unit-tests
+	checkIntv    time.Duration
+	paths        map[string]*pathRecord
+}
+
+// pathRecord holds the internal monitored data for a path.
+// Atomics are used to allow concurrent update/read-access.
+type pathRecord struct {
+	pathStr     string
+	refreshIntv time.Duration
+	modTime     atomic.Value
+	failCount   atomic.Int32
+}
+
+// PathStatus holds status of a monitored path, returned by GetPathStatus()
+type PathStatus struct {
+	PathStr   string
+	ModTime   time.Time
+	FailCount int32
+}
+
+// NewModTimeMonitor returns a new ModTimeMonitor with a
+// background monitoring thread already started
+func NewModTimeMonitor(ctx context.Context, opts ...Option) *ModTimeMonitor {
+	defOpts := newDefaultOptions()
+	for _, opt := range opts {
+		opt(defOpts)
+	}
+
+	mtm := &ModTimeMonitor{
+		log:       defOpts.log,
+		statFS:    defOpts.statFS,
+		checkIntv: defOpts.checkIntv,
+		paths:     make(map[string]*pathRecord),
+	}
+	go mtm.runLoop(ctx)
+	return mtm
+}
+
+// AddMonitoredPath adds a path to be monitored for modtime changes
+func (mtm *ModTimeMonitor) AddMonitoredPath(pathStr string, opts ...Option) error {
+	defOpts := newDefaultOptions()
+	for _, opt := range opts {
+		opt(defOpts)
+	}
+
+	pathStr = CleanPath(pathStr)
+
+	mtm.Lock()
+	defer mtm.Unlock()
+
+	pathRec, found := mtm.paths[pathStr]
+	if found {
+		return ErrDuplicatePath
+	}
+
+	var zeroTime time.Time
+	pathRec = &pathRecord{
+		pathStr:     pathStr,
+		refreshIntv: defOpts.refreshIntv,
+	}
+	pathRec.modTime.Store(zeroTime)
+	mtm.paths[pathStr] = pathRec
+
+	return nil
+}
+
+// GetPathStatus returns the current status of a monitored path
+func (mtm *ModTimeMonitor) GetPathStatus(pathStr string) (*PathStatus, error) {
+	pathStr = CleanPath(pathStr)
+
+	mtm.RLock()
+	defer mtm.RUnlock()
+
+	pathRec, found := mtm.paths[pathStr]
+	if !found {
+		return nil, ErrPathNotFound
+	}
+
+	// Make a copy of status info returned,
+	// do NOT return the actual internal status info
+	// for thread-safety
+	pathSts := &PathStatus{
+		PathStr:   pathRec.pathStr,
+		ModTime:   pathRec.modTime.Load().(time.Time),
+		FailCount: pathRec.failCount.Load(),
+	}
+
+	return pathSts, nil
+}
+
+// checkPathModTimes checks the modtimes of its set of paths
+func (mtm *ModTimeMonitor) checkPathModTimes(ctx context.Context, nowTime time.Time) {
+	mtm.RLock()
+	defer mtm.RUnlock()
+
+	for pathStr, pathRec := range mtm.paths {
+		// When using fs.FS, paths must be unrooted
+		// See https://github.com/golang/go/issues/44279#issuecomment-956251702
+		// See https://pkg.go.dev/io/fs#ValidPath
+		unrooted := strings.TrimLeft(pathStr, "/")
+		fInfo, err := mtm.statFS.Stat(unrooted)
+		if err != nil {
+			// log error, but continue
+			mtm.log(fmt.Sprintf("checkPathModTimes: Stat(%s) err=%s", pathStr, err))
+		} else {
+			newTime := fInfo.ModTime()
+			mtm.log(fmt.Sprintf("checkPathModTimes: Stat(%s) ModTime=%s", pathStr, newTime))
+			pathRec.modTime.Store(newTime)
+		}
+
+		if nowTime.Sub(pathRec.modTime.Load().(time.Time)) > pathRec.refreshIntv {
+			pathRec.failCount.Add(1)
+		} else {
+			pathRec.failCount.Store(0)
+		}
+	}
+}
+
+// runLoop is the background ModTimeMonitor thread that
+// periodically checks its paths' modtimes in a loop.
+// Terminated when context is done (canceled).
+func (mtm *ModTimeMonitor) runLoop(ctx context.Context) {
+	mtm.log("ModTimeMonitor.runLoop: started")
+
+	checkTicker := time.NewTicker(mtm.checkIntv)
+	defer checkTicker.Stop()
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			checkTicker.Stop()
+			break loop
+
+		case curTime := <-checkTicker.C:
+			mtm.checkPathModTimes(ctx, curTime)
+		}
+	}
+
+	mtm.log("ModTimeMonitor.runLoop: terminated")
+}
+
+// CleanPath cleans and trimspaces path strings
+func CleanPath(pathStr string) string {
+	return path.Clean(strings.TrimSpace(pathStr))
+}
---
+++ fsnotify/modtime/modtime_test.go
@@ -0,0 +1,264 @@
+package modtime
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	utilt "github.com/infobloxopen/hotload/util_test"
+)
+
+// TestAgainstUnixFS verifies that the use of io/fs.FS in the implementation
+// works against the real Unix FS
+func TestAgainstUnixFS(t *testing.T) {
+	var zeroTime time.Time
+	ctx, cancelCtxFn := context.WithCancel(context.Background())
+	defer cancelCtxFn()
+
+	// Create ModTimeMonitor that monitors the (default) real host Unix FS
+	mtm := NewModTimeMonitor(ctx,
+		WithCheckInterval(time.Millisecond*200),
+		WithLogger(func(args ...any) {
+			log.Println(args...)
+		}),
+	)
+
+	// Add well-known Unix path to monitor the mod-time of
+	pth := "/dev/null"
+	mtm.AddMonitoredPath(pth,
+		WithRefreshInterval(time.Millisecond*2000),
+	)
+
+	// Give time for ModTimeMonitor background thread to check mod-times
+	time.Sleep(time.Millisecond * 500)
+
+	// Verify valid mod-time has been retrieved
+	sts, err := mtm.GetPathStatus(pth)
+	if err != nil {
+		t.Errorf("GetPathStatus(%s): unexpected err=%s", pth, err)
+	} else {
+		t.Logf("sts=%+v", sts)
+		if zeroTime.After(sts.ModTime) {
+			t.Errorf("GetPathStatus(%s): unexpected ModTime=%+v", pth, sts.ModTime)
+		}
+	}
+
+	// Unfortunately for some reason, os.Chtimes() returns
+	//   "chtimes /dev/null: operation not permitted"
+	// so we can't update /dev/null modtime for additional testing
+
+	// Cancel ctx and give time for background threads to terminate
+	cancelCtxFn()
+	time.Sleep(time.Millisecond * 200)
+}
+
+// TestAgainstMapFS verifies using MapFS mock FS
+func TestAgainstMapFS(t *testing.T) {
+	var zeroTime time.Time
+	ctx, cancelCtxFn := context.WithCancel(context.Background())
+	defer cancelCtxFn()
+
+	// Create MapFS mock FS
+	pth := "/foo/bar"
+	mfs := utilt.NewSafeMapFS()
+	mfs.UpsertMapFile(pth, &fstest.MapFile{ModTime: MustParseRFC3339("2010-04-01T23:07:59Z")})
+
+	checkIntv := time.Millisecond * 200
+
+	// Create ModTimeMonitor that monitors mock FS
+	mtm := NewModTimeMonitor(ctx,
+		WithStatFS(mfs),
+		WithCheckInterval(checkIntv),
+		WithLogger(func(args ...any) {
+			log.Println(args...)
+		}),
+	)
+
+	// Add mock path to monitor the mod-time of.
+	// Notice that the path refreshIntv is the same as checkIntv for this test.
+	mtm.AddMonitoredPath(pth,
+		WithRefreshInterval(checkIntv),
+	)
+
+	// Give time for ModTimeMonitor background thread to check mod-times.
+	// Wait 2 cycles of checks.
+	time.Sleep(2*checkIntv + 10*time.Millisecond)
+
+	// Verify valid mod-time has been retrieved
+	sts, err := mtm.GetPathStatus(pth)
+	if err != nil {
+		t.Errorf("GetPathStatus(%s): unexpected err=%s", pth, err)
+	} else {
+		t.Logf("sts=%+v", sts)
+		if zeroTime.After(sts.ModTime) {
+			t.Errorf("GetPathStatus(%s): unexpected ModTime=%+v", pth, sts.ModTime)
+		}
+		if sts.FailCount != 2 {
+			t.Errorf("GetPathStatus(%s): unexpected FailCount=%+v, expected 2", pth, sts.FailCount)
+		}
+	}
+
+	// Update mock path mod-time
+	mapf, err := mfs.GetMapFile(pth)
+	if err != nil {
+		t.Errorf("GetMapFile(%s): unexpected err=%s", pth, err)
+	}
+	mapf.ModTime = time.Now()
+	mfs.UpsertMapFile(pth, mapf)
+
+	// Give time for ModTimeMonitor background thread to check mod-times.
+	// Wait 1 cycle of checks.
+	time.Sleep(1*checkIntv + 10*time.Millisecond)
+
+	// Verify FailCount is zero because mod-time was updated
+	sts, err = mtm.GetPathStatus(pth)
+	if err != nil {
+		t.Errorf("GetPathStatus(%s): unexpected err=%s", pth, err)
+	} else {
+
+		t.Logf("sts=%+v", sts)
+		if sts.FailCount > 0 {
+			t.Errorf("GetPathStatus(%s): unexpected FailCount=%+v, expected 0", pth, sts.FailCount)
+		}
+	}
+
+	// Give time for ModTimeMonitor background thread to check mod-times.
+	// Wait 1 cycle of checks.
+	time.Sleep(1*checkIntv + 10*time.Millisecond)
+
+	// Verify FailCount is now one
+	sts, err = mtm.GetPathStatus(pth)
+	if err != nil {
+		t.Errorf("GetPathStatus(%s): unexpected err=%s", pth, err)
+	} else {
+
+		t.Logf("sts=%+v", sts)
+		if sts.FailCount != 1 {
+			t.Errorf("GetPathStatus(%s): unexpected FailCount=%+v, expected 1", pth, sts.FailCount)
+		}
+	}
+
+	// Cancel ctx and give time for background threads to terminate
+	cancelCtxFn()
+	time.Sleep(time.Millisecond * 200)
+}
+
+// TestConcurrency verifies thread-safety by spawning multiple
+// threads all running with the same interval.
+// Should be tested with go test -race flag.
+func TestConcurrency(t *testing.T) {
+	var zeroTime time.Time
+	ctx, cancelCtxFn := context.WithCancel(context.Background())
+	defer cancelCtxFn()
+
+	commonIntv := time.Millisecond * 100
+
+	// Create MapFS mock FS
+	mfs := utilt.NewSafeMapFS()
+
+	modTimePaths := []struct {
+		pathStr     string
+		startMtime  time.Time
+		refreshIntv time.Duration
+	}{
+		{
+			pathStr:     "/concurrency/sub10",
+			startMtime:  time.Now(),
+			refreshIntv: time.Millisecond * 800,
+		},
+		{
+			pathStr:     "/concurrency/sub20",
+			startMtime:  zeroTime,
+			refreshIntv: time.Millisecond * 800,
+		},
+		{
+			pathStr:     "/concurrency/sub30",
+			startMtime:  MustParseRFC3339("2010-04-01T23:07:59Z"),
+			refreshIntv: time.Millisecond * 800,
+		},
+	}
+
+	// Add paths to mock FS
+	for _, pathRec := range modTimePaths {
+		mfs.UpsertMapFile(pathRec.pathStr, &fstest.MapFile{ModTime: pathRec.startMtime})
+	}
+
+	// Create ModTimeMonitor that monitors mock FS
+	mtm := NewModTimeMonitor(context.Background(),
+		WithStatFS(mfs),
+		WithCheckInterval(commonIntv),
+		WithLogger(func(args ...any) {
+			log.Println(args...)
+		}),
+	)
+
+	modTimeUpdaterLoop := func(ctx context.Context, t *testing.T, pathStr string, updateIntv time.Duration) {
+		mtm.log(fmt.Sprintf("modTimeUpdaterLoop(%s) started", pathStr))
+		updateTicker := time.NewTicker(updateIntv)
+		defer updateTicker.Stop()
+	loop:
+		for {
+			select {
+			case <-ctx.Done():
+				break loop
+			case curTime := <-updateTicker.C:
+				mapf, err := mfs.GetMapFile(pathStr)
+				if err != nil {
+					t.Errorf("modTimeUpdaterLoop(%s): GetMapFile() err=%s", pathStr, err)
+				} else {
+					mapf.ModTime = curTime
+					mfs.UpsertMapFile(pathStr, mapf)
+				}
+			}
+		}
+		mtm.log(fmt.Sprintf("modTimeUpdaterLoop(%s) terminated", pathStr))
+	}
+
+	modTimeReaderLoop := func(ctx context.Context, t *testing.T, pathStr string, readIntv time.Duration) {
+		mtm.log(fmt.Sprintf("modTimeReaderLoop(%s) started", pathStr))
+		readTicker := time.NewTicker(readIntv)
+		defer readTicker.Stop()
+	loop:
+		for {
+			select {
+			case <-ctx.Done():
+				break loop
+			case <-readTicker.C:
+				sts, err := mtm.GetPathStatus(pathStr)
+				if err != nil {
+					t.Errorf("modTimeReaderLoop(%s): GetPathStatus() err=%s", pathStr, err)
+				} else {
+					t.Logf("sts=%+v", sts)
+				}
+			}
+		}
+		mtm.log(fmt.Sprintf("modTimeReaderLoop(%s) terminated", pathStr))
+	}
+
+	// Add paths to monitor
+	for _, pathRec := range modTimePaths {
+		mtm.AddMonitoredPath(pathRec.pathStr, WithRefreshInterval(pathRec.refreshIntv))
+		go modTimeUpdaterLoop(ctx, t, pathRec.pathStr, commonIntv)
+		go modTimeReaderLoop(ctx, t, pathRec.pathStr, commonIntv)
+	}
+
+	// Give time for background threads to do their thing
+	time.Sleep(time.Millisecond * 3000)
+
+	// Cancel ctx and give time for background threads to terminate
+	cancelCtxFn()
+	time.Sleep(time.Millisecond * 1000)
+}
+
+// MustParseRFC3339 calls time.Parse(time.RFC3339,...),
+// and panics on error, otherwise returns parsed time.Time result
+func MustParseRFC3339(str string) time.Time {
+	t, err := time.Parse(time.RFC3339, str)
+	if err != nil {
+		panic(fmt.Sprintf("time.Parse(%s) err=%s", str, err))
+	}
+	return t
+}
